1e402d4f01735b23ed023b0b4941a11443db144b
spatybr-arch <spatybr@gmail.com>
2026-04-24 11:15:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-const SUPABASE_URL = "https://abiphfpimlztmnlhofae.supabase.co
-";
-const SUPABASE_KEY = "sb_publishable_4yNXtvBC1vJUqgL0HQWaVQ_B8v4RYDH
-";
+const SUPABASE_URL = "https://abiphfpimlztmnlhofae.supabase.co";
+const SUPABASE_KEY = "sb_publishable_4yNXtvBC1vJUqgL0HQWaVQ_B8v4RYDH";
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
